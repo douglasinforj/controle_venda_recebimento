a8fa67ab7fa79edd9dd10c05efecc74fc3913d90
Douglas Silva <douglasitpro@gmail.com>
2025-03-12 02:02:55 -0300

    Adicionando rotas login - logout

--- controle_vendas_app/urls.py
+++ controle_vendas_app/urls.py
@@ -2,5 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home, name='home')
+    path('', views.home, name='home'),
+
+    path("login/", views.CustomLoginView.as_view(), name="login"),
+    path("logout/", views.CustomLogoutView.as_view(), name="logout"),
 ]
