f82fd9374b1dc6e1319fd7eb92055492fbf6a2ba
Douglas Silva <douglasitpro@gmail.com>
2025-03-11 23:30:42 -0300

    Ajuste de conflito com campo venda do models Parcela pis estava com mesmo nome related a models Venda

--- controle_vendas_app/models.py
+++ controle_vendas_app/models.py
@@ -1,5 +1,8 @@
 from django.db import models
 
+from datetime import date, timedelta
+from django.db.models import Q
+
 class Cliente(models.Model):
     nome = models.CharField(max_length=200)
     cpf_cnpj = models.CharField(max_length=15, unique=True)
@@ -26,9 +29,12 @@ class Venda(models.Model):
     parcelado = models.BooleanField(default=False)
     parcelas = models.PositiveIntegerField(default=1)
 
+    def __str__(self):
+        return f"Venda {self.id} - {self.cliente.nome}"
+
 
 class Parcela(models.Model):
-    venda = models.ForeignKey(Venda, on_delete=models.CASCADE, related_name="parcelas")
+    venda = models.ForeignKey(Venda, on_delete=models.CASCADE, related_name="parcelas_venda")
     numero = models.PositiveIntegerField()
     valor = models.DecimalField(max_digits=10, decimal_places=2)
     data_vencimento = models.DateField()
@@ -36,3 +42,21 @@ class Parcela(models.Model):
 
     def __str__(self):
         return f"Parcela {self.numero} - Venda {self.venda.id}"
+    
+
+# Função lista clientes com parcelas vencidas ou próximo de vencimento
+
+def listar_alertas():
+    hoje = date.today()
+    proximo = hoje + timedelta(days=5)
+
+    #busca clientes com parcelas vencidas
+    vencidos = Parcela.objects.filter(data_vencimento__lt=hoje, pago=False)
+
+    #busca clientes com parcelas a vencer em até 5 dias
+    proximo_a_vencer = Parcela.objects.filter(data_vencimento__range=(hoje, proximo), pago=False)
+
+    return {
+        "vencidos": vencidos,
+        "proximo_a_vencer": proximo_a_vencer
+    }
