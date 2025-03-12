192080d48103aa50fafbd46e1254871608ad791c
Douglas Silva <douglasitpro@gmail.com>
2025-03-11 23:44:43 -0300

    Função Rota home

--- controle_vendas_app/views.py
+++ controle_vendas_app/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
 
-# Create your views here.
+from datetime import date, timedelta
+from .models import Parcela
+
+def home(request):
+    hoje = date.today()
+    proximo = hoje + timedelta(days=5)
+
+    vencidos = Parcela.objects.filter(data_venciemnto__lt=hoje, pago=False)
+    proximo_a_vencer = Parcela.objects.filter(data_vencimento__range=(hoje, proximo), pago=False)
+
+    context = {
+        "vencidos": vencidos,
+        "proximo_a_vencer": proximo_a_vencer
+    }
+    return render(request, "controle_vendas_app/home.html", context)
