810f331a67e7a5d3e196ee2c8eaf7652ace8448a
Douglas Silva <douglasitpro@gmail.com>
2025-05-21 10:29:23 -0300

    feat: criando form para produtos

--- controle_vendas_app/forms.py
+++ controle_vendas_app/forms.py
@@ -1,7 +1,13 @@
 from django import forms 
-from .models import Cliente
+from .models import Cliente, Produto
 
 class ClienteForm(forms.ModelForm):
     class Meta:
         model = Cliente
         fields = ['nome', 'cpf_cnpj', 'telefone', 'email', 'endereco']
+
+
+class ProdutoForm(forms.ModelForm):
+    class Meta:
+        model = Produto
+        fields = ['nome', 'descricao', 'preco', 'estoque']
