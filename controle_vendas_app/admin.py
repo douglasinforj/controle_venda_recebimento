549e20fdb124f150c055920fa4c7850f102e15b7
Douglas Silva <douglasitpro@gmail.com>
2025-05-22 00:50:35 -0300

    feat: adicionando Produto e tratamento de imagens no admin

--- controle_vendas_app/admin.py
+++ controle_vendas_app/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from .models import Produto, Categoria, ImagemProduto
 
-# Register your models here.
+class ImagemProdutoInline(admin.TabularInline):
+    model = ImagemProduto
+    extra = 1
+
+@admin.register(Produto)
+class ProdutoAdmin(admin.ModelAdmin):
+    list_display = ('nome', 'categoria', 'preco', 'estoque')
+    inlines = [ImagemProdutoInline]
+
+admin.site.register(Categoria)
