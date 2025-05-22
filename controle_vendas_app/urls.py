44aba4e552746bb56077b984b2cc6438b827dd7c
Douglas Silva <douglasitpro@gmail.com>
2025-05-22 00:54:17 -0300

    feat: adicionando rota para editar produto

--- controle_vendas_app/urls.py
+++ controle_vendas_app/urls.py
@@ -11,5 +11,6 @@ urlpatterns = [
     path('clientes/cadastrar/', views.cadastrar_cliente, name='cadastrar_cliente'),
 
     path('listar_produtos/', views.listar_produtos, name='listar_produtos'), 
-    path('produtos/cadastrar/', views.cadastrar_produto, name='cadastrar_produto')
+    path('produtos/cadastrar/', views.cadastrar_produto, name='cadastrar_produto'),
+    path('produtos/editar/<int:produto_id>/', views.editar_produto, name='editar_produto'),
 ]
