a22b17c4b84d2d3178c175f06b5b254524ace095
Douglas Silva <douglasitpro@gmail.com>
2025-05-22 02:01:27 -0300

    feat:Adicionadno class bootstrap form-control

--- controle_vendas_app/forms.py
+++ controle_vendas_app/forms.py
@@ -11,12 +11,24 @@ class ClienteForm(forms.ModelForm):
 class ProdutoForm(forms.ModelForm):
     class Meta:
         model = Produto
-        fields = ['nome', 'descricao', 'categoria', 'preco_custo', 'preco', 'estoque']     #campo lucro fica de fora, poiis ele não é editavel
+        fields = ['nome', 'descricao', 'categoria', 'preco_custo', 'preco', 'estoque']
+        widgets = {
+            'nome': forms.TextInput(attrs={'class': 'form-control'}),
+            'descricao': forms.Textarea(attrs={'class': 'form-control', 'rows': 3}),
+            'categoria': forms.Select(attrs={'class': 'form-select'}),
+            'preco_custo': forms.NumberInput(attrs={'class': 'form-control'}),
+            #'lucro': forms.NumberInput(attrs={'class': 'form-control'}),
+            'preco': forms.NumberInput(attrs={'class': 'form-control'}),
+            'estoque': forms.NumberInput(attrs={'class': 'form-control'}),
+        }
 
 class ImagemProdutoForm(forms.ModelForm):
     class Meta:
         model = ImagemProduto
         fields = ['imagem']
+        widgets = {
+            'imagem': forms.ClearableFileInput(attrs={'class': 'form-control'}),
+        }
 
     def clean_imagem(self):
         imagem = self.cleaned_data.get('imagem')
